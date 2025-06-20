f68e3403331fab4066cca529fe36606133953f7e
Jannatul Rikta <jannatrikta99@gmail.com>
2025-06-20 16:03:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showHideSearch(hidden_form) {
+	const selectorId = document.getElementById('hidden_form');
+		if (selectorId.style.display == "none") {
+				selectorId.style.display = "block";
+		}else{
+				selectorId.style.display = "none";
+		}
+}
